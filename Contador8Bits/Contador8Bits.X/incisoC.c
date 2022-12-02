fe8c0e6479c145ee5299ce94c9f3ab0a58ceefe5
AdrianGomz <83665825+AdrianGomz@users.noreply.github.com>
2022-12-02 17:50:19 -0600

    Update incisoC.c

--- Contador8Bits/Contador8Bits.X/incisoC.c
+++ Contador8Bits/Contador8Bits.X/incisoC.c
@@ -20,37 +20,27 @@ void main(void) {
     int i = 0b00000000;
     while(1)
     {
-//        Revisar pausa
        if(RD0==1 && RD1==1){
            continue;
        }
-//       Revisar reset
        else if(RD0==0 && RD1==0){
            i=0;
        }
-//       Setear puerto al valor
        PORTB = i;
-//       Revisar si es ascendente
        if (RD1==1){
-//         Caso en que se llegue al valor maximo
            if(i==0b11111111){
                i=0;
            }else{
                i++;
            }
-//      Revisar si es descendente
        }else if(RD0==1){
-//         Caso en el que se llega a 0
            if(i==0){
                i=0b11111111;
            }else{
                i--;
            }
-           
        }
-       //Delay
        __delay_ms(1000);
-          
     }
     return;
 }
